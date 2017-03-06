e4f8503df06f82542fd7106fb85dc878aa0f1cc2
Juan Sebastian Cardona <js.cardona12@uniandes.edu.co>
2017-03-06 10:22:31 -0500

    f

--- PageController.js
+++ PageController.js
@@ -2,7 +2,7 @@ var app = angular.module('yapp', ['ngAnimate','ui.router']);
 
     app.config(function($stateProvider,$urlRouterProvider){
 
-
+        $urlRouterProvider.otherwise("/home");
         $stateProvider
 
             .state('home',{
